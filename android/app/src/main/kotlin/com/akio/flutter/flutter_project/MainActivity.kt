c7d6ab637e34fc342ed737a8f9f706d7b2be5928
Mostafa Ahmed <mostafaahmed@Mostafas-MacBook-Air.local>
2023-07-19 00:25:05 +0300

    This includes all files in the project. The 2 main files are login_screen.dart and main.dart

---
+++ android/app/src/main/kotlin/com/akio/flutter/flutter_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.akio.flutter.flutter_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  var emailController = TextEditingController();
+  var passController = TextEditingController();
+
+  LogInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+        leading: const Icon(Icons.menu),
+        title: const Text(
+          "Sign in",
+          style: TextStyle(fontSize: 20),
+        ),
+        actions: const [Icon(Icons.search)],
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                const Image(
+                  image: NetworkImage(
+                      'https://cdn-icons-png.flaticon.com/512/5087/5087579.png'),
+                  width: 200,
+                  height: 200,
+                ),
+                const Text(
+                  "Sign in",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.indigoAccent),
+                ),
+                const SizedBox(height: 20),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  width: 300,
+                  child: TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.email),
+                          prefixIconColor: Colors.indigoAccent)),
+                ),
+                const SizedBox(height: 20),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  width: 300,
+                  child: TextFormField(
+                    controller: passController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock),
+                        prefixIconColor: Colors.indigoAccent),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                // ignore: sized_box_for_whitespace
+                Container(
+                  color: Colors.indigoAccent,
+                  width: 150,
+                  child: MaterialButton(
+                    onPressed: () {
+                      print(emailController.text);
+                      print(passController.text);
+                    },
+                    child: const Text(
+                      "Sign in",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account?",
+                      style: TextStyle(fontSize: 15),
+                      textAlign: TextAlign.center,
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: const Text("Register Now",
+                            style: TextStyle(
+                                color: Colors.indigoAccent, fontSize: 15),
+                            textAlign: TextAlign.center))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
